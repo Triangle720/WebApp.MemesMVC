13c6c146d06d2783ff43a6a99f8f9865c0fe226d
Przemysław Słoma <przemyslawsloma6@gmail.com>
2020-10-29 20:56:14 +0100

    PicManager / Management buttons fix

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebApp.MemesMVC.Data;
 using WebApp.MemesMVC.Models;
 using WebApp.MemesMVC.Security;
